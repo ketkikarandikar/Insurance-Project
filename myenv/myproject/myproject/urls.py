059fc22ca4dae6e97cfd137c453c266f93cbbee8
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-09-22 14:50:34 +0530

    First Commit

--- myenv/myproject/myproject/urls.py
+++ myenv/myproject/myproject/urls.py
@@ -1,8 +1,11 @@
+
+
+
 """
 URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +18,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from myapp import views as myapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+    path('admin_login/', myapp_views.admin_login, name='admin_login'),
 ]
